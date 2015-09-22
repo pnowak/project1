f0243366acb9a2bd1312fe57f742d1402d74516b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2015-09-22 23:59:06 +0200

    jasmine test begin

---
+++ test/jasmine/spec/expect.js
@@ -0,0 +1,6 @@
+expect(cashRegister to be a object) - cashRegister toBe Object;
+expect(start with zero) - casRegister.total toBeEqual 0;
+expect(me - instancja StaffMember to be a object) - me toBe Object;
+expect(me ma imie) me.name toBeDefined();
+expect(me.discountPercent jest wieksze od zera) me.discountPercent toBeGreaterThan(0);
+expect(cashRegister.scan() return true) cashRegister.scan() toByTruthy;
