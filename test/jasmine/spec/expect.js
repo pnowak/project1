21e7b2266c77447812136073edf9525cea90f63b
pnowak <piotr.nowak.25.02.1978@gmail.com>
2015-09-23 23:57:42 +0200

    testowanie cashRegister object

--- test/jasmine/spec/expect.js
+++ test/jasmine/spec/expect.js
@@ -1,6 +1,61 @@
-expect(cashRegister to be a object) - cashRegister toBe Object;
-expect(start with zero) - casRegister.total toBeEqual 0;
-expect(me - instancja StaffMember to be a object) - me toBe Object;
-expect(me ma imie) me.name toBeDefined();
-expect(me.discountPercent jest wieksze od zera) me.discountPercent toBeGreaterThan(0);
-expect(cashRegister.scan() return true) cashRegister.scan() toByTruthy;
+describe('Cash register', function () {
+
+    function StaffMember(name, discountPercent){
+            this.name = name;
+            this.discountPercent = discountPercent;
+    }
+
+    var me = new StaffMember('Pio', 20);
+
+    var cashRegister = {
+        total: 0,
+        lastTransactionAmount: 0,
+        add: function(itemCost){
+            this.total += (itemCost || 0);
+            this.lastTransactionAmount = itemCost;
+        },
+        scan: function(item, quantity){
+            switch (item){
+            case "eggs": this.add(0.98 * quantity); break;
+            case "milk": this.add(1.23 * quantity); break;
+            case "magazine": this.add(4.99 * quantity); break;
+            case "chocolate": this.add(0.45 * quantity); break;
+            }
+            return true;
+        },
+        voidLastTransaction : function(){
+            this.total -= this.lastTransactionAmount;
+            this.lastTransactionAmount = 0;
+        },
+        applyStaffDiscount: function(employee) {
+            this.total -= this.total * (employee.discountPercent / 100);
+        }
+    };
+
+    it('is a object', function () {
+    	expect(cashRegister).not.toBe(null);
+    });
+
+    it('to include add method', function () {
+        expect(cashRegister.add).toBeDefined();
+    });
+
+    it('property - total - start with zero', function () {
+    	expect(cashRegister.total).toBeEqual(0);
+    });
+
+    it('method - scan - always return true', function () {
+    	expect(cashRegister.scan).toByTruthy();
+    });
+
+    describe('have StaffMember', function () {
+
+    	it('with a name', function () {
+    		expect(me.name).toBeDefined();
+    	});
+
+        it('with method discountPercent which always greater than zero', function () {
+        	expect(me.discountPercent).toBeGreaterThan(0);
+        });
+    });
+});
